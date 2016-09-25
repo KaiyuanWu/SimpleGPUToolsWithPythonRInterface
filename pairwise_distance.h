057ccec1b1edc15c150f7c968a95b820d505aad9
Kaiyuan Wu <kaiwu@buaa.edu.cn>
2016-09-25 21:55:36 +0800

    Update

--- pairwise_distance.h
+++ pairwise_distance.h
@@ -1,2 +1,2 @@
-int pairwise_distance_gpu(float* x, int nrows, int ncols, float* dist);
-int pairwise_distance_gpu(double* x, int nrows, int ncols, double* dist);
+int pairwise_distance_gpu1(float* x, int nrows, int ncols, float* dist);
+int pairwise_distance_gpu2(float* x, int xnrows, int xncols, float* y, int ynrows, int yncols, float* dist);
